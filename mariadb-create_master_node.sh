9568876770fc046b2919c9d6fcd6841c9ef1f0fa
kamil <kamil.lada63@gmail.com>
2024-07-04 23:46:06 +0200

    add pma

--- mariadb-create_master_node.sh
+++ mariadb-create_master_node.sh
@@ -14,7 +14,7 @@ sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.a
 sudo add-apt-repository 'deb [arch=amd64] http://mirror.23media.de/mariadb/repo/10.5/ubuntu focal main'
 
 sudo apt update
-sudo apt install -y mariadb-server
+sudo apt install -y mariadb-server php-mbstring php-zip php-gd phpmyadmin
 
 # Stop MariaDB service
 sudo systemctl stop mariadb
